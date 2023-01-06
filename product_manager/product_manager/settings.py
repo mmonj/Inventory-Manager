e37a4801747723f5247fca757a3ebd533ef88abd
mmonj <mmonj@users.noreply.github.com>
2023-01-06 14:51:37 -0500

    add environment variable

--- product_manager/product_manager/settings.py
+++ product_manager/product_manager/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -107,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = os.environ['TZ']
 
 USE_I18N = True
 
