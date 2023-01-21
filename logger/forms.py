4de20dea9db9a7665038a73a97c7f724e60142cd
mmonj <mmonj@users.noreply.github.com>
2023-01-21 17:30:18 -0500

    add import page

--- logger/forms.py
+++ logger/forms.py
@@ -2,3 +2,9 @@ from django import forms
 
 class NewStoresForm(forms.Form):
     stores_text = forms.CharField(widget=forms.Textarea())
+
+
+class ImportJsonDataFiles(forms.Form):
+    territory_info_json = forms.FileField()
+    store_distribution_data_json = forms.FileField()
+    product_names_json = forms.FileField()
