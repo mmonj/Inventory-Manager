dce8f700c9da5bdaf5ab4a2f5438eba5e9b0bf48
mmonj <mmonj@users.noreply.github.com>
2023-01-22 13:15:38 -0500

    implement import functionality

--- logger/forms.py
+++ logger/forms.py
@@ -6,5 +6,5 @@ class NewStoresForm(forms.Form):
 
 class ImportJsonDataFiles(forms.Form):
     territory_info_json = forms.FileField()
-    store_distribution_data_json = forms.FileField()
     product_names_json = forms.FileField()
+    store_distribution_data_json = forms.FileField()
--- products/helpers.py
+++ products/helpers.py
@@ -1,5 +1,6 @@
 import sys
 from . import models
+from datetime import datetime, timezone
 
 def printerr(output, print_type=False):
     '''
@@ -44,6 +45,45 @@ def import_territories(territory_info):
 
 def import_products(products_info: dict):
     for parent_company, products in products_info.items():
-        parent_company, is_new_parent_company = models.BrandParentCompany.objects.get_or_create(short_name=parent_company)
+        parent_company, __ = models.BrandParentCompany.objects.get_or_create(short_name=parent_company)
         for upc, info in products.items():
-            product, is_new_product = models.Product.objects.get_or_create(upc=upc, name=info.get('fs_name'), parent_company=parent_company)
+            product, __ = models.Product.objects.get_or_create(upc=upc, name=info.get('fs_name'), parent_company=parent_company)
+
+
+def import_distribution_data(store_distribution_data: dict):
+    for store_name, products in store_distribution_data.items():
+        store, __ = models.Store.objects.get_or_create(name=store_name)
+        for upc, distribution_data in products.items():
+            product, __ = models.Product.objects.get_or_create(upc=upc)
+            product_addition = get_product_addition(distribution_data, product, store)
+
+
+def get_product_addition(distribution_data: dict, product: models.Product, store: models.Store) -> models.ProductAddition:
+    product_addition, is_new_product_addition = models.ProductAddition.objects.get_or_create(
+        product=product, 
+        store=store
+    )
+
+    if not is_new_product_addition:
+        return product_addition
+
+    product_addition.date_added = datetime.fromtimestamp( distribution_data.get('time_added', 0), timezone.utc )
+    product_addition.date_last_scanned = get_utc_datetime( distribution_data.get('date_scanned') )
+    product_addition.is_carried = distribution_data.get('instock', False)
+
+    product_addition.save(update_fields=['date_added', 'date_last_scanned', 'is_carried'])
+    return product_addition
+
+
+def get_utc_datetime(datetime_str: str) -> datetime:
+    if datetime_str is None:
+        return datetime.fromtimestamp(0, timezone.utc)
+
+    import pytz
+    datetime_object = datetime.strptime(datetime_str, "%Y-%m-%d at %I:%M:%S %p")
+
+    est = pytz.timezone("EST")
+    local_time = est.localize(datetime_object)
+    utc_time = local_time.astimezone(pytz.utc)
+
+    return utc_time
