01ea688700113fd987fd03abbd995d0527d890ee
mmonj <mmonj@users.noreply.github.com>
2023-02-05 20:35:40 -0500

    implement api view update_product_names

--- api/views.py
+++ api/views.py
@@ -1,23 +1,35 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 
 from products import models
-from products.serializers import ProductSerializer
+from products.serializers import ProductAdditionSerializer
+from rest_framework.permissions import IsAuthenticated
+
 
 @api_view(['POST'])
-def test(request, *args, **kwargs):
-    """
-    DRF API View
-    """
+@permission_classes([IsAuthenticated])
+def update_product_names(request):
+    def get_product_name(upc: str, products: list):
+        for product_info in products:
+            if product_info['upc'] == upc:
+                return product_info['name']
+        return None
+    
+    store_name = request.data.get('store_name')
+    client_name = request.data.get('client_name')
+    upcs = [p['upc'] for p in request.data.get('products')]
 
-    product = models.Product.objects.first()
-    serialized_product = ProductSerializer(product)
-    return Response(serialized_product.data)
+    # products with no name
+    products = models.Product.objects.filter(upc__in=upcs, name=None)
+    parent_company, _ = models.BrandParentCompany.objects.get_or_create(short_name=client_name)
 
-    # serializer = ProductSerializer(data=request.data)
-    # if serializer.is_valid(raise_exception=True):
-    #     # instance = serializer.save()
-    #     # instance = form.save()
-    #     print(serializer.data)
-    #     return Response(serializer.data)
-    return Response({"invalid": "not good data"}, status=400)
+    for product in products:
+        product.parent_company = parent_company
+        product.name = get_product_name(product.upc, request.data.get('products'))
+    
+    products.bulk_update(products, ['parent_company', 'name'])
+        
+
+    product_additions = models.ProductAddition.objects.filter(store__name=store_name, is_carried=True, product__upc__in=upcs)
+    product_additions_serializer = ProductAdditionSerializer(product_additions, many=True)
+    return Response(product_additions_serializer.data)
