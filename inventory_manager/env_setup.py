b09323388cccc12352d092253df34c5e822ea5bf
mmonj <mmonj@users.noreply.github.com>
2023-02-20 23:39:07 -0500

    update STATIC_ROOT path

--- inventory_manager/env_setup.py
+++ inventory_manager/env_setup.py
@@ -5,7 +5,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 STATIC_URL = "/static/"
-STATIC_ROOT = BASE_DIR / "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles/"
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media/"
