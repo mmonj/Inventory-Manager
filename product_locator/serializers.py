1f22ca59718b5f9f317ce52aa1daf41ceaf9cd53
mmonj <mmonj@users.noreply.github.com>
2023-03-08 00:34:40 -0500

    add HomeLocationSerializer

--- product_locator/serializers.py
+++ product_locator/serializers.py
@@ -6,3 +6,14 @@ class StoreSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Store
         fields = ["id", "name"]
+
+
+class HomeLocationSerializer(serializers.ModelSerializer):
+    planogram = serializers.SerializerMethodField()
+
+    class Meta:
+        model = models.HomeLocation
+        fields = ["name", "planogram"]
+
+    def get_planogram(self, home_location):
+        return home_location.planogram.name
