877111bb8ff563ad309fab214795b10af8e414d2
mmonj <mmonj@users.noreply.github.com>
2023-06-27 21:43:26 -0400

    prepare Layout.tsx basic config

--- client/components/Layout.tsx
+++ client/components/Layout.tsx
@@ -3,6 +3,11 @@ import React from "react";
 import { Context } from "@reactivated";
 import { Helmet } from "react-helmet-async";
 
+import { Navbar } from "./Navbar";
+
+import "@client/styles/bs-material-dark.css";
+import "@client/styles/shared.css";
+
 interface Props {
   title: string;
   children: React.ReactNode;
@@ -18,28 +23,15 @@ export const Layout = (props: Props) => {
         <meta charSet="utf-8" />
         <title>{props.title}</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        {/* <link
-                    rel="icon"
-                    href={`${context.STATIC_URL}favicon.ico`}
-                    type="image/x-icon"
-                />
-                <link
-                    rel="stylesheet"
-                    type="text/css"
-                    href={`${context.STATIC_URL}admin/css/fonts.css`}
-                />
-                <link
-                    rel="stylesheet"
-                    type="text/css"
-                    href={`${context.STATIC_URL}dist/index.css`}
-                /> */}
+        <link rel="icon" href={`${context.STATIC_URL}favicon.ico`} type="image/x-icon" />
+        <link rel="stylesheet" type="text/css" href={`${context.STATIC_URL}admin/css/fonts.css`} />
+        <link rel="stylesheet" type="text/css" href={`${context.STATIC_URL}dist/index.css`} />
         <script defer crossOrigin="anonymous" src={`${context.STATIC_URL}dist/index.js`} />
       </Helmet>
       <header>
-        <div>Header here</div>
+        <Navbar />
       </header>
       <main>{props.children}</main>
-      <footer>Footer here</footer>
     </>
   );
 };
