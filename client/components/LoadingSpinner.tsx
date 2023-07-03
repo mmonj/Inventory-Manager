394e72ee0abc93ce2f82ac61d8c4d472722f6c70
mmonj <mmonj@users.noreply.github.com>
2023-07-02 23:47:45 -0400

    add component

--- client/components/LoadingSpinner.tsx
+++ client/components/LoadingSpinner.tsx
@@ -4,14 +4,13 @@ import Spinner from "react-bootstrap/Spinner";
 
 interface Props {
   size?: "sm" | undefined;
+  color?: string;
 }
 
-export function LoadingSpinner({ size }: Props) {
+export function LoadingSpinner({ size, color }: Props) {
   return (
-    <div className="d-flex justify-content-center">
-      <Spinner animation="border" variant="primary" role="status" size={size}>
-        <span className="visually-hidden">Loading...</span>
-      </Spinner>
-    </div>
+    <Spinner className={color} animation="border" variant="primary" role="status" size={size}>
+      <span className="visually-hidden">Loading...</span>
+    </Spinner>
   );
 }
