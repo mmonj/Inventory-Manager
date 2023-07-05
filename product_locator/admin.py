9cb74c9c9403f60c2654ebcc6076912e4a3e507e
mmonj <mmonj@users.noreply.github.com>
2023-07-04 23:12:16 -0400

    include model types in admin.ModelAdmin declarations

--- product_locator/admin.py
+++ product_locator/admin.py
@@ -1,26 +1,26 @@
 from django.contrib import admin
-from . import models
+from .models import Store, Planogram, HomeLocation, Product
 
 
-class PlanogramAdmin(admin.ModelAdmin):
-    search_fields = ['name']
-    list_display = ['name', 'store']
-    list_filter = ['store']
+class PlanogramAdmin(admin.ModelAdmin[Planogram]):
+    search_fields = ["name"]
+    list_display = ["name", "store"]
+    list_filter = ["store"]
 
 
-class HomeLocationAdmin(admin.ModelAdmin):
-    search_fields = ['name', 'planogram__name']
-    list_display = ['name', 'planogram']
-    list_filter = ['planogram', "planogram__store"]
+class HomeLocationAdmin(admin.ModelAdmin[HomeLocation]):
+    search_fields = ["name", "planogram__name"]
+    list_display = ["name", "planogram"]
+    list_filter = ["planogram", "planogram__store"]
 
 
-class ProductAdmin(admin.ModelAdmin):
-    search_fields = ['upc', 'name', 'home_locations__name']
-    list_display = ['upc', 'name']
-    list_filter = ['home_locations__planogram', 'home_locations__planogram__store']
+class ProductAdmin(admin.ModelAdmin[Product]):
+    search_fields = ["upc", "name", "home_locations__name"]
+    list_display = ["upc", "name"]
+    list_filter = ["home_locations__planogram", "home_locations__planogram__store"]
 
 
-admin.site.register(models.Store)
-admin.site.register(models.Planogram, PlanogramAdmin)
-admin.site.register(models.HomeLocation, HomeLocationAdmin)
-admin.site.register(models.Product, ProductAdmin)
+admin.site.register(Store)
+admin.site.register(Planogram, PlanogramAdmin)
+admin.site.register(HomeLocation, HomeLocationAdmin)
+admin.site.register(Product, ProductAdmin)
