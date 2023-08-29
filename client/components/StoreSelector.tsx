737a1a2112218bb8ab3a2372dd3e775e404f3df1
mmonj <mmonj@users.noreply.github.com>
2023-08-29 00:39:55 -0400

    implement ReactSelect searchable dropdown menu

--- client/components/StoreSelector.tsx
+++ client/components/StoreSelector.tsx
@@ -1,4 +1,6 @@
-import React, { forwardRef, useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
+
+import Select from "react-select";
 
 import { IFieldRep, IStore } from "@client/templates/StockTrackerScanner";
 
@@ -8,6 +10,8 @@ type BaseProps = {
 
 type StoreSelectorProps = {
   stores: IStore[];
+  selectedStore: StoreSelectOption | null;
+  setSelectedStore: React.Dispatch<React.SetStateAction<StoreSelectOption | null>>;
 };
 
 type Props = BaseProps &
@@ -22,31 +26,43 @@ type Props = BaseProps &
       }
   );
 
-function StoreSelectorInner({ stores }: StoreSelectorProps, ref: React.Ref<HTMLSelectElement>) {
+interface StoreSelectOption {
+  value: number;
+  label: string;
+}
+
+function StoreSelector({ stores, selectedStore, setSelectedStore }: StoreSelectorProps) {
+  const options: StoreSelectOption[] = stores.map((store) => ({
+    value: store.pk,
+    label: store.name ?? "-no-store-name-",
+  }));
+
   return (
-    <p id="store-select-container" className="mb-3">
+    <div id="store-select-container" className="my-1 mb-3">
       <label htmlFor="store-select" className="form-label">
         Store
       </label>
-      <select ref={ref} className="d-block col-12 form-select" name="store-id" required>
-        {stores.map((store) => (
-          <option key={store.pk} value={store.pk}>
-            {store.name}
-          </option>
-        ))}
-      </select>
-    </p>
+
+      <Select
+        required={true}
+        placeholder="Select a store"
+        value={selectedStore}
+        onChange={setSelectedStore}
+        options={options}
+        classNamePrefix="react-select"
+      />
+    </div>
   );
 }
-const StoreSelector = forwardRef(StoreSelectorInner);
 
 export function FieldRepStoreSelector(props: Props) {
   const [listedStores, setListedStores] = useState<IStore[]>([]);
+  const [selectedStore, setSelectedStore] = useState<StoreSelectOption | null>(null);
   const fieldRepRef = useRef<HTMLSelectElement>(null);
-  const storePkRef = useRef<HTMLSelectElement>(null);
 
   function onFieldRepChange() {
     if (props.propType !== "fieldReps") return;
+    setSelectedStore(() => null);
 
     const fieldRepPk = fieldRepRef.current?.value;
     if (fieldRepPk === undefined) throw new Error("Field Rep Pk is undefined");
@@ -59,10 +75,10 @@ export function FieldRepStoreSelector(props: Props) {
 
   function onSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
-    const storePk = storePkRef.current?.value;
+    const storePk = selectedStore?.value;
     if (storePk === undefined) throw new Error("Store Pk is undefined");
 
-    props.handleStoreSubmission(storePk);
+    props.handleStoreSubmission(storePk.toString());
   }
 
   useEffect(() => {
@@ -87,7 +103,8 @@ export function FieldRepStoreSelector(props: Props) {
               ref={fieldRepRef}
               className="form-select"
               id="field-representative-select"
-              required>
+              required
+            >
               {props.field_reps.map((field_rep) => (
                 <option key={field_rep.pk} value={field_rep.pk}>
                   {field_rep.name}
@@ -96,7 +113,11 @@ export function FieldRepStoreSelector(props: Props) {
             </select>
           </p>
         )}
-        <StoreSelector stores={listedStores} ref={storePkRef} />
+        <StoreSelector
+          stores={listedStores}
+          selectedStore={selectedStore}
+          setSelectedStore={setSelectedStore}
+        />
       </fieldset>
       <button type="submit" className="btn btn-primary col-12 my-2">
         Submit
