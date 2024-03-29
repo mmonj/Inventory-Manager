175896ac53bdbb65d9287ff6d85a24332116c8e1
mmonj <mmonj@users.noreply.github.com>
2024-03-28 23:12:08 -0400

    add new error templates

--- server/urls.py
+++ server/urls.py
@@ -31,5 +31,5 @@ urlpatterns = [
     *static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
 ]
 
-handler404 = "homepage.views.error404"
-handler500 = "homepage.views.error500"
+# handler404 = "homepage.views.error404"
+# handler500 = "homepage.views.error500"
