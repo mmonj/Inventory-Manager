c92379ea976fa526976fed0df96f45bd232c7945
mmonj <mmonj@users.noreply.github.com>
2025-04-29 01:33:59 -0400

    move CommonModel to server utils

--- server/utils/typedefs.py
+++ server/utils/typedefs.py
@@ -1,6 +1,10 @@
 from dataclasses import dataclass
 from typing import Generic, Literal, TypeVar, Union
 
+from django.contrib.auth.models import User
+from django.db import models
+from django.http import HttpRequest
+
 T = TypeVar("T")
 E = TypeVar("E")
 
@@ -18,6 +22,18 @@ class TFailure(Generic[E]):
 
 
 TResult = Union[TSuccess[E], TFailure[T]]
+
+
+class CommonModel(models.Model):
+    datetime_created = models.DateTimeField(auto_now_add=True, editable=False)
+    datetime_modified = models.DateTimeField(auto_now=True, editable=False)
+
+    class Meta:
+        abstract = True
+
+
+class StaffRequest(HttpRequest):
+    user: User
 
 
 __all__ = ["TResult"]
