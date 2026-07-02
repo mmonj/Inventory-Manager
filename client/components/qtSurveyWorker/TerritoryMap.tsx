de1fa586f9e9f21a922cae3ff67cc9ffc77f8e08
mmonj <mmonj@users.noreply.github.com>
2026-07-02 19:06:34 -0400

    update map tile layer and attribution in TerritoryMap component

--- client/components/qtSurveyWorker/TerritoryMap.tsx
+++ client/components/qtSurveyWorker/TerritoryMap.tsx
@@ -207,8 +207,8 @@ export default function TerritoryMap({ groupedByStore }: Props) {
   return (
     <MapContainer center={defaultCenter} zoom={12} style={{ height: "100%", width: "100%" }}>
       <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution="© OpenStreetMap contributors"
+        url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+        attribution='© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors © <a href="https://carto.com/attributions">CARTO</a>'
       />
       {validEntries.map(([siteId, group]) => (
         <Marker
